0420292bcde5651b2dc3c2c525be4dc789a7f29c
BOUDEFFAZAKARYA <boudeffazack@gmail.com>
2022-12-19 15:43:42 +0100

    adding the pass

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .setTitle('Example')
     .setDescription('The cats API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('exemple')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,15 +6,60 @@ import {
   Param,
   Patch,
   Delete,
-  UseGuards
+  UseGuards,
+  UseInterceptors, UploadedFile, Request, StreamableFile
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 import { ProductsService } from './products.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+
+import { Observable, of } from 'rxjs';
+
+import { catchError, map, tap } from 'rxjs/operators';
+
+
+
+
+
+
+
+import {
+  ApiBearerAuth,
+
+} from '@nestjs/swagger';
+
+import path = require('path');
+import { Product } from './product.model';
+import { SampleDto } from './dtos/sampleDto';
+import { join } from 'path';
+import { createReadStream } from 'fs';
+
+
+
+export const storage = {
+  storage: diskStorage({
+    destination: './uploads',
+    filename: (req, file, cb) => {
+      const filename: string = path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+      const extension: string = path.parse(file.originalname).ext;
+
+      cb(null, `${filename}${extension}`)
+    }
+  })
+};
+
+
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) { }
+
+
+
+
 
   @Post()
   async addProduct(
@@ -29,7 +74,45 @@ export class ProductsController {
     );
     return { id: generatedId };
   }
+
+
+  @UseInterceptors(FileInterceptor('file', storage))
+  @Post('upload')
+  async uploadFile(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+    //@Body('image') image: string,
+
+   // @UploadedFile() file,
+    @Request() req ,
+
+  ) {
+
+    
+   // console.log(file);
+
+
+    const product: Product = req.product;
+
+
+
+    return {
+      prodTitle,
+      
+      
+    };
+  }
+
+
+
+
+
   @UseGuards(JwtAuthGuard)
+
+
+
+  @ApiBearerAuth()
 
   @Get()
   async getAllProducts() {
@@ -48,8 +131,10 @@ export class ProductsController {
     @Body('title') prodTitle: string,
     @Body('description') prodDesc: string,
     @Body('price') prodPrice: number,
+    @Body('price') prodImage: string,
+
   ) {
-    await this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+    await this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice, prodImage);
     return null;
   }
 
@@ -58,4 +143,20 @@ export class ProductsController {
     await this.productsService.deleteProduct(prodId);
     return null;
   }
+
+
+
+
+
+  @Get('imges')
+  getFile() {
+    const file = createReadStream(join(process.cwd(), 'uploads'));
+    return new StreamableFile(file);
+
+  }
+
+
+
 }
+
+
